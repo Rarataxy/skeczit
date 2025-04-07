b13f81bd5b9b3032fe4f5fc9dca83a8f591a87a8
rarataxy <rarataxy>
2025-04-08 01:51:27 +0200

    Added room routes

---
+++ server/routes/roomRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { createRoom, addPlayerToRoom, getPlayersInRoom, startGame, updateScore } = require('../models/gameModel');
+const router = express.Router();
+
+router.get("/rooms", (req, res) => {
+    res.send('List of rooms\nblah\blah\blah\n>;v');
+})
+
+router.post("/rooms/create", (req, res) => {
+    res.send('Room created c;');
+})
+
+router.post("/rooms/:room_id/join", (req, res) => {
+    res.send('Join a room >.<');
+})
+
+router.post("/rooms/:room_id/leave", (req, res) => {
+    res.send('Left a room ;C');
+})
+
+router.post("/rooms/:room_id/start", (req, res) => {
+    res.send('Game started!!!');
+})
+
+router.get("/rooms/:room_id/status", (req, res) => {
+    res.send('Room status :|');
+})
+
+router.post("/rooms/:room_id/next_round", (req, res) => {
+    res.send('The furure is now old man');
+})
+
+router.post("/rooms/:room_id/end_game", (req, res) => {
+    res.send('Game OVER');
+})
