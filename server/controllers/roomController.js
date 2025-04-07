26656444089e94296bd5d10e176de35e5b784704
rarataxy <rarataxy>
2025-04-08 01:51:27 +0200

    added functionality to join a room

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -15,7 +15,31 @@ const createRoom = asyncHandler(async (req, res, next) => {
     res.send("Room created successfully")
 })
 
+const joinRoom = asyncHandler(async (req, res, next) => {
+    const { room_id } = req.params;
+    const { player, password } = req.body;
+
+    const room = await Room.findOne({ roomId: room_id });
+
+    if (!room) {
+      return res.status(404).send('Room not found');
+    }
+
+    if (room.password && room.password !== password) {
+      return res.status(403).send('Incorrect password');
+    }
+
+    if (room.currentPlayers >= room.maxPlayers) {
+      return res.status(400).send('Room is full');
+    }
+
+    room.players.push(player);
+    room.currentPlayers++;
+    await room.save();
+    res.send(`Player ${player} joined room ${room_id}`);
+})
 
 module.exports = {
-    createRoom
+    createRoom,
+    joinRoom
 };
