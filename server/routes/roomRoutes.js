34d61bf73c40c2bc9f68ff6c2b19dc21b6f0edc0
rarataxy <rarataxy>
2025-04-08 01:51:27 +0200

    fixed route paths

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,35 +1,34 @@
 const express = require('express');
+const roomController = require('../controllers/roomController');
 const router = express.Router();
 
-router.get("/rooms", (req, res) => {
+router.get("/", (req, res) => {
     res.send('List of rooms\nblah\blah\blah\n>;v');
 })
 
-router.post("/rooms/create", (req, res) => {
-    res.send('Room created c;');
-})
+router.post("/create", roomController.createRoom);
 
-router.post("/rooms/:room_id/join", (req, res) => {
+router.post("/:room_id/join", (req, res) => {
     res.send('Join a room >.<');
 })
 
-router.post("/rooms/:room_id/leave", (req, res) => {
+router.post("/:room_id/leave", (req, res) => {
     res.send('Left a room ;C');
 })
 
-router.post("/rooms/:room_id/start", (req, res) => {
+router.post("/:room_id/start", (req, res) => {
     res.send('Game started!!!');
 })
 
-router.get("/rooms/:room_id/status", (req, res) => {
+router.get("/:room_id/status", (req, res) => {
     res.send('Room status :|');
 })
 
-router.post("/rooms/:room_id/next_round", (req, res) => {
+router.post("/:room_id/next_round", (req, res) => {
     res.send('The furure is now old man');
 })
 
-router.post("/rooms/:room_id/end_game", (req, res) => {
+router.post("/:room_id/end_game", (req, res) => {
     res.send('Game OVER');
 })
 
