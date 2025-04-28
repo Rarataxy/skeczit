bba655487ef3afa7cd286a4e1e62e34d865f7ae4
rarataxy <rarataxy>
2025-04-28 22:47:29 +0200

    Disabled CORS so the react app can actually get responses

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes.js');
 const roomRoutes = require('./routes/roomRoutes.js');
 const app = express();
 app.use(express.json()); 
+app.use(cors());
 
-const port = 3000;
+const port = 3002;
 
 app.use('/user', userRoutes);
 app.use('/rooms', roomRoutes);
